b1c4dceab5c49340b0560bc80ca8cd675350cb6e
Houda Jawad <houdajawad@gmail.com>
2023-05-11 16:17:28 -0700

    Add App class component and state to render our app

--- src/App/App.js
+++ src/App/App.js
@@ -1,1 +1,24 @@
-import movieData from '../movieData'
+import movieData from '../movieData';
+import React, {Component} from 'react';
+import Movies from '../Movies/Movies'
+import '../App/App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            movies: movieData.movies
+        }
+        console.log(this.state.movies)
+    }
+
+render() {
+    return (
+        <main className='App'> 
+        <h1>Hello</h1>
+        <Movies movies={this.state.movies}/>
+        </main>
+    )
+}
+}
+export default App;
