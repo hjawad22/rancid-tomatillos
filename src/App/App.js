dafff6a6ca59c19f3213f831ed78b913b8e901d0
Randy Cisneros <rlcisneros90@gmail.com>
2023-05-12 07:39:23 -0600

    Add functionality to display a single movie and return back to the original state

--- src/App/App.js
+++ src/App/App.js
@@ -8,17 +8,41 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            movies: movieData.movies
+            movies: movieData.movies,
+            movie: null
         }
         console.log(this.state.movies)
     }
 
+    getMovieById = (id) => {
+        const filteredMovie = this.state.movies.find(movie => movie.id === id)
+        this.setState({movie: filteredMovie})
+        console.log("filteredMovies", filteredMovie)
+    }
+
+    switchState = () => {
+        this.setState({movie: null})
+    }
+
 render() {
+    if(this.state.movie) {
+        return (
+        <>
+        <Nav/>
+        <main className='movie-description'>
+            {console.log(this.state.movie)}
+        <h1>{this.state.movie.title}</h1>
+        <img className='single-image' src={this.state.movie.poster_path} alt={this.state.movie.title}/>
+        <button className='back-button' onClick={() => this.switchState()}>Back to Movies</button>
+        </main>
+        </>
+        )
+    }
     return (
         <>
         <Nav/>
-        <main className='App'> 
-        <Movies movies={this.state.movies}/>
+        <main className='App'>
+        <Movies movies={this.state.movies} getMovieById={this.getMovieById}/>
         </main>
         </>
     )}
