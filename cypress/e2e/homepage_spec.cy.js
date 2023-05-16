44a6daf6f586f102ed76e25ed2cec3bb0fd7627b
Randy Cisneros <rlcisneros90@gmail.com>
2023-05-15 18:45:20 -0600

    Refac: to clean up code by using beforeEach and use mockData for testing

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -1,45 +1,41 @@
 describe('Homepage Spec', () => {
-  it('should allow a user to visit the application homepage', () => {
-    cy.visit('http://localhost:3000/')
-  })
-
-  it('homepage should have a nav bar', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('Nav')
-  })
-
-  it('should display a title on the homepage', () => {
-    cy.visit('http://localhost:3000/')
-    cy.contains('Flick Finder')
-  })
-
-  it('should display all movies', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('img')
-  })
-
-  it('should display a titles for all movies', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('h3')
-  })
-
-  it('should display a release date for all movies', () => {
-    cy.visit('http://localhost:3000/')
-    cy.contains('Release Date:').should('be.visible')
-  })
-
-  it('should display a rating for all movies', () => {
-    cy.visit('http://localhost:3000/')
-    cy.contains('Rating')
-  })
-
-  it('should display a button for each movie', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('button')
-  })
-
-  it('button should say View Details', () => {
-    cy.visit('http://localhost:3000/')
-    cy.contains('View Details')
-  })
-})
+  beforeEach(() => {
+     cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2//movies/", {
+       statusCode: 200,
+       fixture: "movies.json"
+     })
+     .visit("http://localhost:3000/")})
+ 
+ 
+   it('homepage should have a nav bar', () => {
+     cy.get('Nav')
+   })
+ 
+   it('should display a title on the homepage', () => {
+     cy.contains('Flick Finder')
+   })
+ 
+   it('should display all movies', () => {
+     cy.get('img')
+   })
+ 
+   it('should display a titles for all movies', () => {
+     cy.get('h3')
+   })
+ 
+   it('should display a release date for all movies', () => {
+     cy.contains('Release Date:').should('be.visible')
+   })
+ 
+   it('should display a rating for all movies', () => {
+     cy.contains('Rating')
+   })
+ 
+   it('should display a button for each movie', () => {
+     cy.get('button')
+   })
+ 
+   it('button should say View Details', () => {
+     cy.contains('View Details')
+   })
+ })
