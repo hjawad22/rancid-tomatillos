7dd943a10e002e5089d5bf353ce26ed50fc81f0d
Houda Jawad <houdajawad@gmail.com>
2023-05-18 19:13:38 -0700

    Add error handling messaging test

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -1,41 +1,52 @@
 describe('Homepage Spec', () => {
   beforeEach(() => {
-     cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2//movies/", {
-       statusCode: 200,
-       fixture: "movies.json"
-     })
-     .visit("http://localhost:3000/")})
- 
- 
-   it('homepage should have a nav bar', () => {
-     cy.get('Nav')
-   })
- 
-   it('should display a title on the homepage', () => {
-     cy.contains('Flick Finder')
-   })
- 
-   it('should display all movies', () => {
-     cy.get('img')
-   })
- 
-   it('should display a titles for all movies', () => {
-     cy.get('h3')
-   })
- 
-   it('should display a release date for all movies', () => {
-     cy.contains('Release Date:').should('be.visible')
-   })
- 
-   it('should display a rating for all movies', () => {
-     cy.contains('Rating')
-   })
- 
-   it('should display a button for each movie', () => {
-     cy.get('button')
-   })
- 
-   it('button should say View Details', () => {
-     cy.contains('View Details')
-   })
- })
+    cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2//movies/", {
+      statusCode: 200,
+      fixture: "movies"
+    }).visit("http://localhost:3000/");
+  });
+
+  it('homepage should have a nav bar with a title', () => {
+    cy.get('Nav').contains('Flick Finder');
+  });
+
+  it('should display titles and images for all movies', () => {
+    cy.get('.movie-card')
+      .should('have.length', 40)
+      .get('h3');
+  });
+
+  it('should display a release date for all movies', () => {
+    cy.get('.movies-container')
+      .contains('Release Date:');
+  });
+
+  it('should display a button for each movie', () => {
+    cy.get('.movie-card')
+      .get('.view-button');
+  });
+
+  it('should have a view button that takes user to that movie\'s specific details', () => {
+    cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2//movies/436270", {
+      statusCode: 200,
+      fixture: "movie"
+    });
+
+    cy.get('.view-button')
+      .contains('View Details')
+      .click()
+      .url()
+      .should('eq', 'http://localhost:3000/436270');
+  });
+
+  it('should display error message', () => {
+    cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2//movies/", {
+      statusCode: 404,
+      fixture: "movie"
+    });
+
+    cy.get('.error')
+      .contains('Oops! We seem to be having some technical issues, please try again later!')
+  });
+
+});
