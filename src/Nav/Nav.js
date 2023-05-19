ee8d2f840dcabd51fbfcd48655bc177079033827
Houda Jawad <houdajawad@gmail.com>
2023-05-18 19:17:40 -0700

    Add refactoring to code

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import '../Nav/Nav.css'
+import '../Nav/Nav.css';
 
 function Nav() {
-    return (
-        <nav className = 'Nav'>
-            <h1 className='title'>Flick Finder</h1>
-        </nav>
-    )
+  return (
+    <nav className='Nav'>
+      <h1 className='title'>Flick Finder</h1>
+    </nav>
+  );
 }
 
 export default Nav;
