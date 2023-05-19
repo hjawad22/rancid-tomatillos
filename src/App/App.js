f645e3cd654444f9fcfeaad5169a558fb4ced659
Houda Jawad <houdajawad@gmail.com>
2023-05-18 19:32:30 -0700

    Refactor code

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Movies from '../Movies/Movies';
 import Nav from '../Nav/Nav';
 import SingleMovie from '../SingleMovie/SingleMovie';
 import '../App/App.css';
-import { Route} from 'react-router-dom'
+import { Route } from 'react-router-dom';
 
 class App extends Component {
     constructor() {
@@ -11,39 +11,41 @@ class App extends Component {
         this.state = {
             movies: [],
             errorMessage: ''
-        }
+        };
     }
 
     componentDidMount = () => {
-        fetch("https://rancid-tomatillos.herokuapp.com/api/v2//movies/")
-        .then(res => {
-           if(!res.ok) {
-               throw new Error('Failed to fetch movies')
-           }
-           return res.json()})
-        .then(moviesData => this.setState({
-           movies: moviesData.movies
-        }))
-        .catch(error => {
-           console.error("There was a problem with the fetch", error)
-           this.setState({
-            errorMessage: `Error: ${error.message}`
-           })
-        })
-    }
+        fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies/")
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('Oops! We seem to be having some technical issues, please try again later!');
+                }
+                return res.json();
+            })
+            .then(moviesData => this.setState({
+                movies: moviesData.movies,
+                errorMessage: ''
+            }))
+            .catch(error => {
+                console.error("There was a problem with the fetch", error);
+                this.setState({
+                    errorMessage: `${error.message}`
+                });
+            });
+    };
 
     render() {
         return (
             <>
-            <Nav />
-            <span className="error">{this.state.errorMessage}</span>
-            <main className='App'>
-              <Route exact path='/' render={() => (<Movies movies={this.state.movies} getMovieById={this.getMovieById} />)} />
-              <Route path="/:movieId" render={({ match }) => (<SingleMovie movie={match.params.movieId}/>)} />
-            </main>
-          </>
+                <Nav />
+                <h1 className="error">{this.state.errorMessage}</h1>
+                <main className='App'>
+                    <Route exact path='/' render={() => (<Movies movies={this.state.movies} getMovieById={this.getMovieById} />)} />
+                    <Route path="/:movieId" render={({ match }) => (<SingleMovie movie={parseInt(match.params.movieId)} />)} />
+                </main>
+            </>
         );
-      }
+    }
 }
 
 export default App;
